6419249202ae33b805fa9bdeca2d766135b43267
NadezhdaPogodina <pogodinadia@inbox.ru>
2022-12-16 15:27:51 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String passport;
+    public String subscription;
+    public FormDate birthday;
+
+
+}
